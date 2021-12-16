173318e31f117ac26048f65eeae3c617338041c4
Katarzyna Kuich <neokatrina@gmail.com>
2021-12-16 22:45:36 +0100

    added rendered templates

--- movie_catalogue/main.py
+++ movie_catalogue/main.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def homepage():
-    return render_template("index.html")
+    movies = ['tytul filmu1','tytul filmu2''tytul filmu3']
+    return render_template("homepage.html", movies=movies)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
