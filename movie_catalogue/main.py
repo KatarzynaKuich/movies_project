40509bd24b62d88246a0711f9cb41c410a82843f
Katarzyna Kuich <neokatrina@gmail.com>
2021-12-17 09:25:23 +0100

    added screenshot

--- movie_catalogue/main.py
+++ movie_catalogue/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    movies = ['tytul filmu1','tytul filmu2''tytul filmu3']
+    movies = ['tytul filmu1','tytul filmu2','tytul filmu3','tytul filmu4','tytul filmu5']
     return render_template("homepage.html", movies=movies)
 
 
